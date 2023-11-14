da0c0cd05ef9b622966305d26da547b3dcc3c871
Franceeeee <francescomaria.mancinelli@mail.polimi.it>
2023-11-14 17:17:38 +0100

    modifica fecondità.r in intro_B.r

--- main/Analisi introduttiva/Intro_B.R
+++ main/Analisi introduttiva/Intro_B.R
@@ -1,9 +1,12 @@
+# INTRO B
+
 ####Popolazione italiana####
+# creazine del dataset nati per 1000 donne dal 2002 al 2019 vs età delle donne
 
 library(readr)
 fecondita <- read_delim("Analisi Introduttiva/Fecondita-totale-2000-2021.csv", 
                         delim = ",", escape_double = FALSE, trim_ws = TRUE)
-years <- 2000:2021
+years <- 2002:2021
 eta <- 17:50
 fac <- matrix(rep(0, length(years)*length(eta)), nrow = length(years), ncol = length(eta))
 a <- 1
@@ -18,8 +21,16 @@ for (j in 1:length(eta))
 colnames(fac) <- eta
 row.names(fac) <- years
 
+# plot dei dati
+# Define the number of colors in your series
+n_colors <- length(years)
+# Create a color palette that smoothly transitions from red to blue
+color_palette <- colorRampPalette(c("black", "green"))(n_colors)
+
 x11()
-matplot(t(fac), type='l', col=1:length(years))
+matplot(eta,t(fac), type='l', col=color_palette)
+legend('topright', fill=color_palette, legend=years)
+
 
 ####Proviamo l'approccio functional e vediamo che succede - smoothing####
 library(fda)
@@ -43,18 +54,17 @@ for (m in orders)
 }
 View(gcv)
 min <- min(gcv[-which(gcv==0)])
-which(gcv == min, arr.ind=TRUE)
+which(gcv == min, arr.ind=TRUE) 
+# m = 2 order = 13, prendere ordine 2 non ha senso.
+# scegliamo allora la prima base dispari di ordine sufficientemente grande (m = 5) e numero di basi 10
 
 ##Optimal : m = 6, nbasis = 10
 
-basis <- create.bspline.basis(rangeval=range(abscissa), nbasis=10, norder = 6)
+basis <- create.bspline.basis(rangeval=range(abscissa), nbasis=10, norder = 5)
 basismat <- eval.basis(abscissa, basis)
 est_coef = lsfit(basismat, observations, intercept=FALSE)$coef
 Xsp0 <- basismat %*% est_coef
-# Define the number of colors in your series
-n_colors <- 22
-# Create a color palette that smoothly transitions from red to blue
-color_palette <- colorRampPalette(c("black", "green"))(n_colors)
+
 x11()
 matplot(Xsp0, type='l', col=color_palette)
 legend('topright', fill=color_palette, legend=years)
@@ -68,12 +78,14 @@ Xss1 <- eval.fd(abscissa, Xss$fd, Lfd=1)
 Xss2 <- eval.fd(abscissa, Xss$fd, Lfd=2)
 matplot(Xss0, type='l')
 matplot(Xss1, type='l')
-matplot(Xss2, type='l', col=color_palette)  #Molto interessante
+quartz()
+matplot(Xss2, type='l', col=color_palette)  #Potenzialmente interessante
+abline(h=0, col = 'red')
 
 ####Nonparametric - depth####
 library(roahd)
 data <- fData(eta, t(Xsp0))
-med <- median_fData(data)
+med <- median_fData(data) #anche se non ha molto senso prendere in considerazione la mediana di time series
 int <- seq(med$t0, med$tP, by=med$h)
 x11()
 plot(data, type='l')
@@ -85,3 +97,21 @@ epi <- MEI(data)
 plot(years, hypo, type='l')  #Quante linee ci sono sopra a quella in particolare, indice di alteza overall della curva
 #Indice di bassezza overall della fertilità
 plot(years, epi, type='l')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
